529bcc303ac5b8dfbc1a7923f08e246daa747854
grota1122 <81812241+grota1122@users.noreply.github.com>
2021-04-02 14:26:26 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.config = {
+    "token":"ODI2OTIyMTU3MzU5MTA0MTAw.YGThRQ.sV3Nm5wmUxndhE-AVfKQp1DojK8",
+    "game": "mtasa",
+    "serverIP": "145.239.236.14",
+    "port": "22018",
+    "refreshInteval": 60000,
+    "channelText": "mta-serwer: ",
+    "channelID": "826649776413212772",
+}
