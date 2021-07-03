a0376f883bae7a4ad5d209fe616293f467747bf2
grota1122 <81812241+grota1122@users.noreply.github.com>
2021-07-03 09:31:24 +0200

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	kanalkomend: "828688488873590836", //id kanalu z komendami
+	kanalwspolprac: "828678510078984212", // id kanalu z partnerstrwami
+	dbhost: "137.74.6.118", // ip do bazy
+	dbport: "3306", //port bazy
+	dbuser: "db_68005", //nazwa użyt bazy
+	dbpass: "bittorent55", //haslo bazy
+	dbname: "db_68005", //nazwa bazy
+	activitytype: "Online:", // custom czyli costomowa albo online czyli w activity piszesz np. Online: i będzie wyświetlac Online: 0/50
+	activity: "aktualnie:", // tutaj jak dajesz custom piszesz byle co a jak online to po wiadomosci będzie 0/50
+	statushost: "54.38.202.159", //host mta
+	statusport: "21292", // port mta
+	typnagrody: "auto", // auto albo kasa
+	modelauta: 502, // model auta z mtawiki
+	nagrodailosc: "1", //ilosc kasy jezeli hajsiwko
+    token: "ODI2OTIyMTU3MzU5MTA0MTAw.YGThRQ.ed8KS4M4ca7DhRgCqALpw788xpk", // token bota
+    prefix: "!",
+}
---
+++ src/index.js
@@ -0,0 +1,332 @@
+'use strict';
+var _0xd241 = ["setFooter", "length", "createdAt", "png", "' WHERE did='", "shift", "./config/config.js", "split", "then", "11274QVfCLc", "B\u0142\u0105d: Poda\u0142e\u015b zbyt du\u017co argument\u00f3w (Maksymanlnie: 1)", "B\u0142\u0105d: Podana warto\u015b\u0107 nie jest cyfr\u0105: ", "createConnection", "STREAMING", "67174fCstXJ", "601906pvkPHG", "user", "207413RIMjvM", "gamedig", "Rozpocz\u0119to proces ", "warn", "delete", "B\u0142\u0105d: Brak argumentu (Podaj warto\u015b\u0107: Kod weryfikacyjny)",
+"388966PggNNG", "Nieprawid\u0142owa komenda!", "custom", " minut\nIlo\u015b\u0107 pieni\u0119dzy w banku: ", "B\u0142\u0105d: Nie ma takiego kodu w bazie danych!", "trim", "setTitle", "send", "log", "UPDATE discord_connect SET avatarurl='", "Twoje konto discord jest ju\u017c po\u0142\u0105czone z innym kontem MTA!", "Od\u015bwie\u017cenie avataru nieudane!", "add", "setColor", " avatar mozesz nadpisywa\u0107 co godzine!", "INSERT INTO pystories_vehicles (model,ownedPlayer,mileage,registered,parking,engine) VALUES (",
+"296456RvnFcr", "\nW nagrod\u0119 otrzymujesz dodatkowo: ", "Login: ", "online", "setActivity", "B\u0142\u0105d: Avatar mo\u017cesz od\u015bwie\u017ca\u0107 jedynie poprzez wiadomo\u015b\u0107 prywatn\u0105 z botem!", ".\nAby ujrze\u0107 sw\u00f3j avatar i pozby\u0107 si\u0119 znaku wodnego u g\u00f3ry\nekranu, nale\u017cy zrobi\u0107 reconnect!", "setTimestamp", "https://www.twitch.tv/thenitrozyniak", "Error. Nie znaleziono takiego serwera!", "mtasa", "\nZ kontem mta o serialu ", "avatarurl", "B\u0142\u0105d: \u0141\u0105czy\u0107 konto mo\u017cesz jedynie poprzez wiadomo\u015b\u0107 prywatn\u0105 z botem!",
+"Informacje o graczu o SID: ", "channel", "displayAvatarURL", "tag", "SELECT * FROM discord_connect WHERE sid=", "auto", " godzin ", "bot", "To konto MTA jest pod\u0142\u0105czone z kontem discord", "info", ",discordconnected='TAK' WHERE kod='", "Wyst\u0105pi\u0142 b\u0142\u0105d", "SELECT * FROM pystories_users WHERE kod='", "bank_money", "slice", "red", "B\u0142\u0105d: Ta komenda mo\u017ce by\u0107 u\u017cyta tylko i wy\u0142\u0105czne na wsp\u00f3\u0142prace!", "register_serial", "Uda\u0142o ci si\u0119 po\u0142\u0105czy\u0107 konto discord o ID ",
+"maxplayers", "SELECT * FROM pystories_users WHERE id=", "message", "1bTqfyi", "ready", "setDescription", "59995BQxbpg", "startsWith", "discordconnected", "login", ",0,'true',1,'diesel')", "Informacje o bocie", " zanim wy\u015blesz nast\u0119pn\u0105 komend\u0119 odczekaj 10 sekund!", "INSERT INTO discord_connect (did,sid,serial,avatarurl) VALUES ('", "7peYUaY", "PLN\nKonto po\u0142\u0105czone z discordem: ", "kasa", "','", "trunc", "SELECT * FROM discord_connect WHERE did='", "raw", "Taka komenda nie istnieje! Wpisz !help.",
+"debug", "author", "help", "numplayers", "\nCzas sp\u0119dzony: ", "\n \n ", "content", "Po\u0142\u0105czenie nieudane!", "query", "has", "username", "Wykonano przez: ", "UPDATE pystories_users SET bank_money=bank_money+", "hours", "reply", "Bot nie dzia\u0142a tu tylko na kanale komendy!", "Cooldown", "Od\u015bwie\u017cenie udane!", "https://cdn.discordapp.com/attachments/764573595640528976/786229187303571466/unknown.png", "push", "discord.js", "chalk", "mysql", "catch", " ", "/", "", "!", "on",
+"toLocaleLowerCase", "id", "@", "polacz", "'", "SELECT * FROM discord_connect WHERE serial='", "did", "Po\u0142\u0105czono konto!", "')", ",", "avatar", "Twoje konto discord nie jest po\u0142\u0105czone z kontem MTA!", "Zr\u00f3b reconnect w grze aby ujrze\u0107 sw\u00f3j pi\u0119kny avatar!", "B\u0142\u0105d: Tutaj mo\u017cesz u\u017cywa\u0107 tylko komendy !polacz,!avatar", "odetnij", "http://futuremta.pl/odetnij.gif", "B\u0142\u0105d: Brak argumentu (Podaj warto\u015b\u0107: SID)", "B\u0142\u0105d: Nie znaleziono gracza o podanym SID: ",
+"setThumbnail", "**Autor**: vAnimo\n**Wersja**: v1.16\n**Prefix:** !\n**Dost\u0119pne komendy:**\n- info\n- polacz\n- avatar\n- odetnij", "error"];
+var _0x13e4 = [_0xd241[0], _0xd241[1], _0xd241[2], _0xd241[3], _0xd241[4], _0xd241[5], _0xd241[6], _0xd241[7], _0xd241[8], _0xd241[9], _0xd241[10], _0xd241[11], _0xd241[12], _0xd241[13], _0xd241[14], _0xd241[15], _0xd241[16], _0xd241[17], _0xd241[18], _0xd241[19], _0xd241[20], _0xd241[21], _0xd241[22], _0xd241[23], _0xd241[24], _0xd241[25], _0xd241[26], _0xd241[27], _0xd241[28], _0xd241[29], _0xd241[30], _0xd241[31], _0xd241[32], _0xd241[33], _0xd241[34], _0xd241[35], _0xd241[36], _0xd241[37], _0xd241[38],
+_0xd241[39], _0xd241[40], _0xd241[41], _0xd241[42], _0xd241[43], _0xd241[44], _0xd241[45], _0xd241[46], _0xd241[47], _0xd241[48], _0xd241[49], _0xd241[50], _0xd241[51], _0xd241[52], _0xd241[53], _0xd241[54], _0xd241[55], _0xd241[56], _0xd241[57], _0xd241[58], _0xd241[59], _0xd241[60], _0xd241[61], _0xd241[62], _0xd241[63], _0xd241[64], _0xd241[65], _0xd241[66], _0xd241[67], _0xd241[68], _0xd241[69], _0xd241[70], _0xd241[71], _0xd241[72], _0xd241[73], _0xd241[74], _0xd241[75], _0xd241[76], _0xd241[77],
+_0xd241[78], _0xd241[79], _0xd241[80], _0xd241[81], _0xd241[82], _0xd241[83], _0xd241[84], _0xd241[85], _0xd241[86], _0xd241[87], _0xd241[88], _0xd241[89], _0xd241[90], _0xd241[91], _0xd241[92], _0xd241[93], _0xd241[94], _0xd241[95], _0xd241[96], _0xd241[97], _0xd241[98], _0xd241[99], _0xd241[100], _0xd241[101], _0xd241[102], _0xd241[103], _0xd241[104], _0xd241[105], _0xd241[106], _0xd241[107], _0xd241[108], _0xd241[109], _0xd241[110], _0xd241[111], _0xd241[112]];
+var _0x53af = function searchSelect2(totalExpectedResults, entrySelector) {
+totalExpectedResults = totalExpectedResults - 331;
+var _0xf536x5 = _0x13e4[totalExpectedResults];
+return _0xf536x5;
+};
+var _0x5456c7 = _0x53af;
+(function(canCreateDiscussions, data) {
+var toMonths = _0x53af;
+for (; !![];) {
+try {
+var lastScriptData = parseInt(toMonths(371)) * parseInt(toMonths(423)) + -parseInt(toMonths(362)) + parseInt(toMonths(387)) + parseInt(toMonths(434)) * parseInt(toMonths(426)) + -parseInt(toMonths(357)) + -parseInt(toMonths(365)) + -parseInt(toMonths(363));
+if (lastScriptData === data) {
+break;
+} else {
+canCreateDiscussions[_0xd241[113]](canCreateDiscussions[_0xd241[5]]());
+}
+} catch (_0x4656b5) {
+canCreateDiscussions[_0xd241[113]](canCreateDiscussions[_0xd241[5]]());
+}
+}
+})(_0x13e4, 217620);
+var _require = require(_0xd241[114]);
+var Client = _require.Client;
+var MessageEmbed = _require.MessageEmbed;
+var chalk = require(_0xd241[115]);
+var Gamedig = require(_0x5456c7(366));
+var _require2 = require(_0x5456c7(354));
+var kanalkomend = _require2.kanalkomend;
+var kanalwspolprac = _require2.kanalwspolprac;
+var dbhost = _require2.dbhost;
+var dbport = _require2.dbport;
+var dbuser = _require2.dbuser;
+var dbpass = _require2.dbpass;
+var dbname = _require2.dbname;
+var activitytype = _require2.activitytype;
+var activity = _require2.activity;
+var statushost = _require2.statushost;
+var statusport = _require2.statusport;
+var typnagrody = _require2.typnagrody;
+var modelauta = _require2.modelauta;
+var nagrodailosc = _require2.nagrodailosc;
+var token = _require2.token;
+var prefix = _require2.prefix;
+var client = new Client;
+var talkedRecently = new Set;
+var antyavatar = new Set;
+var mysql = require(_0xd241[116]);
+var con = mysql[_0x5456c7(360)]({
+"host" : dbhost,
+"port" : dbport,
+"user" : dbuser,
+"password" : dbpass,
+"database" : dbname
+});
+function getPlayers() {
+var get = _0x5456c7;
+if (activitytype === get(390)) {
+Gamedig[get(337)]({
+"type" : get(397),
+"host" : statushost,
+"port" : statusport
+})[get(356)](function(same) {
+var rel2Mstr = get;
+client[_0xd241[16]][rel2Mstr(391)](activity + _0xd241[118] + same[rel2Mstr(440)][rel2Mstr(332)] + _0xd241[119] + same[rel2Mstr(420)], {
+"type" : _0xd241[13],
+"url" : rel2Mstr(395)
+});
+})[_0xd241[117]](function(canCreateDiscussions) {
+var getConsoleMethod = get;
+console[getConsoleMethod(379)](getConsoleMethod(396));
+});
+}
+if (activitytype === get(373)) {
+client[get(364)][get(391)](_0xd241[120] + activity, {
+"type" : get(361),
+"url" : get(395)
+});
+}
+}
+client[_0xd241[122]](_0x5456c7(424), function() {
+var getConsoleMethod = _0x5456c7;
+console[getConsoleMethod(379)](chalk[getConsoleMethod(416)](getConsoleMethod(367) + client[getConsoleMethod(364)][getConsoleMethod(404)] + _0xd241[121]));
+setInterval(function() {
+getPlayers();
+}, 3E4);
+}), client[_0xd241[122]](_0x5456c7(422), function(message) {
+var peek = _0x5456c7;
+var id = message.author;
+var guild = message.guild;
+if (id[peek(408)]) {
+return;
+}
+if (!message[_0xd241[100]][peek(427)](prefix)) {
+return;
+}
+var data = message[peek(335)][peek(415)](prefix[peek(349)])[peek(376)]()[peek(355)](/ +/g);
+var Maybe = data[peek(353)]()[_0xd241[123]]();
+if (talkedRecently[peek(338)](message[_0xd241[95]][_0xd241[124]])) {
+var artistTrack = (new MessageEmbed)[_0xd241[29]](_0xd241[110])[_0xd241[36]](16711680)[peek(425)](_0xd241[125] + message[_0xd241[95]][peek(404)] + peek(432))[peek(348)](peek(340) + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[_0xd241[54]][peek(378)](artistTrack);
+return;
+} else {
+talkedRecently[_0xd241[35]](message[peek(443)][_0xd241[124]]);
+setTimeout(function() {
+var c = peek;
+talkedRecently[c(369)](message[c(443)][_0xd241[124]]);
+}, 1E4);
+}
+if (Maybe === _0xd241[126]) {
+if (guild) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(412))[peek(384)](16711680)[peek(425)](peek(400))[peek(348)](peek(340) + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[_0xd241[21]](message);
+message[peek(402)][peek(378)](artistTrack);
+return;
+}
+if (data[0] === undefined) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(412))[peek(384)](16711680)[peek(425)](peek(370))[peek(348)](peek(340) + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[_0xd241[54]][_0xd241[30]](artistTrack);
+return;
+}
+if (data[1] !== undefined) {
+var artistTrack = (new MessageEmbed)[_0xd241[29]](peek(412))[_0xd241[36]](16711680)[peek(425)](peek(358))[peek(348)](_0xd241[105] + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[_0xd241[54]][peek(378)](artistTrack);
+return;
+}
+con[_0xd241[102]](peek(413) + data[0] + _0xd241[127], function(isSlidingUp, canCreateDiscussions) {
+var c = peek;
+if (isSlidingUp) {
+throw isSlidingUp;
+}
+if (canCreateDiscussions[_0xd241[1]] < 1) {
+var artistTrack = (new MessageEmbed)[_0xd241[29]](c(336))[c(384)](16711680)[c(425)](c(375))[c(348)](c(340) + message[c(443)][_0xd241[56]])[c(394)](message[_0xd241[2]]);
+message[c(402)][c(378)](artistTrack);
+return;
+}
+con[c(337)](_0xd241[128] + canCreateDiscussions[0][c(418)] + _0xd241[127], function(canCreateDiscussions, message) {
+var ce = c;
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+con[_0xd241[102]](ce(439) + message[_0xd241[95]][_0xd241[124]] + _0xd241[127], function(canCreateDiscussions, msg) {
+var c = ce;
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+if (msg[c(349)] >= 1) {
+if (_0xd241[120] + msg[0][_0xd241[129]] === _0xd241[120] + message[c(443)][_0xd241[124]]) {
+var artistTrack = (new MessageEmbed)[_0xd241[29]](c(336))[c(384)](16711680)[c(425)](c(381))[_0xd241[0]](c(340) + message[c(443)][c(404)])[c(394)](message[c(350)]);
+message[_0xd241[54]][_0xd241[30]](artistTrack);
+return;
+}
+}
+if (message[_0xd241[1]] >= 1) {
+var artistTrack = (new MessageEmbed)[c(377)](c(336))[c(384)](16711680)[c(425)](c(409))[_0xd241[0]](c(340) + message[c(443)][c(404)])[c(394)](message[c(350)]);
+message[c(402)][c(378)](artistTrack);
+return;
+}
+if (message[c(349)] < 1) {
+var artistTrack = (new MessageEmbed)[c(377)](_0xd241[130])[c(384)](16678656)[_0xd241[77]](c(419) + message[c(443)][_0xd241[124]] + c(398) + canCreateDiscussions[0][_0xd241[70]] + c(388) + typnagrody + c(393))[c(348)](c(340) + message[c(443)][c(404)])[c(394)](message[c(350)]);
+message[_0xd241[54]][_0xd241[30]](artistTrack);
+con[_0xd241[102]](c(433) + message[_0xd241[95]][_0xd241[124]] + c(437) + canCreateDiscussions[0][_0xd241[124]] + c(437) + canCreateDiscussions[0][c(418)] + _0xd241[89] + message[c(443)][c(403)]({
+"format" : _0xd241[3],
+"size" : 512
+}) + _0xd241[131], function(canCreateDiscussions, isSlidingUp) {
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+return;
+});
+if (typnagrody === c(436)) {
+con[_0xd241[102]](c(341) + nagrodailosc + c(411) + data[0] + _0xd241[127], function(canCreateDiscussions, isSlidingUp) {
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+return;
+});
+}
+if (typnagrody === c(406)) {
+con[c(337)](c(413) + data[0] + _0xd241[127], function(isSlidingUp, canCreateDiscussions) {
+var recordName = c;
+if (isSlidingUp) {
+throw isSlidingUp;
+}
+con[recordName(337)](recordName(386) + modelauta + _0xd241[132] + canCreateDiscussions[0][_0xd241[124]] + recordName(430), function(canCreateDiscussions, isSlidingUp) {
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+return;
+});
+});
+}
+}
+});
+});
+});
+return;
+}
+if (Maybe === _0xd241[133]) {
+if (guild) {
+var artistTrack = (new MessageEmbed)[peek(377)](_0xd241[64])[peek(384)](16711680)[_0xd241[77]](peek(392))[peek(348)](peek(340) + message[_0xd241[95]][_0xd241[56]])[_0xd241[46]](message[peek(350)]);
+message[peek(369)](message);
+message[peek(402)][peek(378)](artistTrack);
+return;
+}
+if (antyavatar[_0xd241[103]](message[peek(443)][_0xd241[124]])) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(345))[peek(384)](16711680)[peek(425)](_0xd241[125] + message[peek(443)][peek(404)] + peek(385))[_0xd241[0]](peek(340) + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[peek(402)][peek(378)](artistTrack);
+return;
+} else {
+antyavatar[peek(383)](message[peek(443)][_0xd241[124]]);
+setTimeout(function() {
+var c = peek;
+antyavatar[c(369)](message[c(443)][_0xd241[124]]);
+}, 36E4);
+}
+con[peek(337)](peek(439) + message[peek(443)][_0xd241[124]] + _0xd241[127], function(isSlidingUp, message) {
+var c = peek;
+if (isSlidingUp) {
+throw isSlidingUp;
+}
+if (message[c(349)] < 1) {
+var artistTrack = (new MessageEmbed)[_0xd241[29]](c(382))[c(384)](16711680)[_0xd241[77]](_0xd241[134])[c(348)](c(340) + message[c(443)][c(404)])[c(394)](message[c(350)]);
+message[c(402)][c(378)](artistTrack);
+return;
+}
+if (message[_0xd241[1]] >= 1) {
+con[c(337)](c(380) + message[c(443)][c(403)]({
+"format" : c(351),
+"size" : 512
+}) + c(352) + message[c(443)][_0xd241[124]] + _0xd241[127], function(canCreateDiscussions, isSlidingUp) {
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+});
+var artistTrack = (new MessageEmbed)[c(377)](c(346))[c(384)](16678656)[c(425)](_0xd241[135])[c(348)](c(340) + message[_0xd241[95]][_0xd241[56]])[_0xd241[46]](message[c(350)]);
+message[c(402)][_0xd241[30]](artistTrack);
+return;
+}
+});
+return;
+}
+if (!guild) {
+var artistTrack = (new MessageEmbed)[_0xd241[29]](peek(412))[peek(384)](16711680)[peek(425)](_0xd241[136])[peek(348)](_0xd241[105] + message[peek(443)][peek(404)])[peek(394)](message[_0xd241[2]]);
+message[peek(402)][peek(378)](artistTrack);
+return;
+}
+if (Maybe === _0xd241[137]) {
+if (message[peek(402)][_0xd241[124]] !== kanalwspolprac) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(412))[peek(384)](16711680)[_0xd241[77]](peek(417))[peek(348)](peek(340) + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[peek(402)][peek(378)](artistTrack);
+return;
+}
+message[_0xd241[21]](message);
+message[peek(402)][_0xd241[30]](_0xd241[138]);
+return;
+}
+if (message[peek(402)][_0xd241[124]] !== kanalkomend) {
+message[peek(343)](peek(344));
+return;
+}
+if (Maybe === peek(410)) {
+if (data[0] === undefined) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(412))[peek(384)](16711680)[peek(425)](_0xd241[139])[_0xd241[0]](peek(340) + message[peek(443)][_0xd241[56]])[peek(394)](message[_0xd241[2]]);
+message[_0xd241[54]][_0xd241[30]](artistTrack);
+return;
+}
+if (data[1] !== undefined) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(412))[peek(384)](16711680)[_0xd241[77]](peek(358))[peek(348)](peek(340) + message[peek(443)][peek(404)])[peek(394)](message[peek(350)]);
+message[peek(402)][peek(378)](artistTrack);
+return;
+}
+if (isNaN(data[0])) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(412))[peek(384)](16711680)[peek(425)](peek(359) + data[0])[peek(348)](peek(340) + message[peek(443)][peek(404)])[_0xd241[46]](message[_0xd241[2]]);
+message[_0xd241[54]][peek(378)](artistTrack);
+return;
+}
+con[peek(337)](peek(421) + data[0], function(canCreateDiscussions, boardManager) {
+var prevIndexTotal = peek;
+if (canCreateDiscussions) {
+throw canCreateDiscussions;
+}
+con[prevIndexTotal(337)](prevIndexTotal(405) + data[0], function(isSlidingUp, visibleTiles) {
+var POS2TILE = prevIndexTotal;
+if (isSlidingUp) {
+throw isSlidingUp;
+}
+if (boardManager[POS2TILE(349)] < 1) {
+var artistTrack = (new MessageEmbed)[POS2TILE(377)](POS2TILE(412))[_0xd241[36]](16711680)[POS2TILE(425)](_0xd241[140] + data[0])[POS2TILE(348)](_0xd241[105] + message[POS2TILE(443)][POS2TILE(404)])[POS2TILE(394)](message[_0xd241[2]]);
+message[POS2TILE(402)][POS2TILE(378)](artistTrack);
+return;
+}
+if (boardManager[POS2TILE(349)] >= 1) {
+if (visibleTiles[POS2TILE(349)] < 1) {
+var artistTrack = (new MessageEmbed)[POS2TILE(377)](POS2TILE(401) + data[0])[POS2TILE(384)](16678656)[_0xd241[77]](POS2TILE(389) + boardManager[0][POS2TILE(429)] + POS2TILE(333) + Math[POS2TILE(438)](boardManager[0][POS2TILE(342)] / 60) + POS2TILE(407) + (boardManager[0][POS2TILE(342)] - Math[POS2TILE(438)](boardManager[0][POS2TILE(342)] / 60) * 60) + _0xd241[26] + boardManager[0][POS2TILE(414)] + _0xd241[87] + boardManager[0][_0xd241[80]] + POS2TILE(334))[POS2TILE(348)](_0xd241[105] +
+message[_0xd241[95]][POS2TILE(404)])[_0xd241[141]](POS2TILE(347))[_0xd241[46]](message[POS2TILE(350)]);
+message[POS2TILE(402)][_0xd241[30]](artistTrack);
+return;
+}
+if (visibleTiles[POS2TILE(349)] >= 1) {
+var artistTrack = (new MessageEmbed)[POS2TILE(377)](POS2TILE(401) + data[0])[POS2TILE(384)](16678656)[POS2TILE(425)](POS2TILE(389) + boardManager[0][POS2TILE(429)] + POS2TILE(333) + Math[POS2TILE(438)](boardManager[0][POS2TILE(342)] / 60) + POS2TILE(407) + (boardManager[0][POS2TILE(342)] - Math[POS2TILE(438)](boardManager[0][POS2TILE(342)] / 60) * 60) + POS2TILE(374) + boardManager[0][POS2TILE(414)] + POS2TILE(435) + boardManager[0][POS2TILE(428)] + _0xd241[99])[POS2TILE(348)](POS2TILE(340) +
+message[POS2TILE(443)][POS2TILE(404)])[_0xd241[141]](_0xd241[120] + visibleTiles[0][POS2TILE(399)])[POS2TILE(394)](message[_0xd241[2]]);
+message[_0xd241[54]][POS2TILE(378)](artistTrack);
+return;
+}
+}
+});
+});
+return;
+}
+if (Maybe === peek(331)) {
+var artistTrack = (new MessageEmbed)[peek(377)](peek(431))[_0xd241[36]](16678656)[peek(425)](_0xd241[142])[peek(348)](_0xd241[105] + message[peek(443)][peek(339)])[peek(394)](message[peek(350)]);
+message[peek(402)][_0xd241[30]](artistTrack);
+return;
+}
+var artistTrack = (new MessageEmbed)[peek(377)](peek(372))[_0xd241[36]](16711680)[peek(425)](peek(441))[peek(348)](peek(340) + message[_0xd241[95]][peek(339)])[peek(394)](message[peek(350)]);
+message[_0xd241[54]][peek(378)](artistTrack);
+}), client[_0xd241[122]](_0x5456c7(442), function() {
+}), client[_0xd241[122]](_0x5456c7(368), function() {
+}), client[_0xd241[122]](_0xd241[143], function() {
+}), client[_0x5456c7(429)](token);
